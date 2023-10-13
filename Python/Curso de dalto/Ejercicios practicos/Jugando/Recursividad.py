4b9cb260daa4108e2be891e8591eff0dc8ca9a9a
SrWalbert <walberttrejo@gmail.com>
2023-10-12 19:55:46 -0700

    Se añadio el archivo de funciones recursivas

--- Python/Curso de dalto/Ejercicios practicos/Jugando/Recursividad.py
+++ Python/Curso de dalto/Ejercicios practicos/Jugando/Recursividad.py
@@ -1,15 +1,22 @@
 def factorial(n: int) -> int:
-    if n == 0:
-        return 1  # Caso base: 0! = 1
+    if n < 0:
+        raise ValueError("El número debe ser no negativo.")
+    elif n == 0:
+        return 1
     else:
-        return n * factorial(n - 1)  # Caso recursivo: n! = n * (n - 1)!
+        return n * factorial(n - 1)
 
 
 while True:
-    numero = input("Ingresa un numero entero para cualcular su factorial: ")
     try:
-        int(numero)
-        if type(numero) is int:
-            print(factorial(numero))
-    except:
-        print("Tu numero no es entero")
+        numero = input("Ingresa un número entero para calcular su factorial: ")
+        numero = int(numero)  # Debes asignar el resultado a una variable.
+
+        print(
+            factorial(numero)
+        )  # Debes imprimir o hacer algo con el resultado de la función.
+
+    except ValueError:
+        print("Tu número no es entero o es negativo.")
+    except Exception as e:
+        print(f"Ocurrió un error inesperado: {e}")
