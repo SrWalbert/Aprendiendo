b83a803a60479d1a84cea719363199bcf6747248
SrWalbert <walberttrejo@gmail.com>
2023-10-13 18:00:14 -0700

    Cambios de formato

--- C#/Escuela/Segundo parcial/Practica 3.cs
+++ C#/Escuela/Segundo parcial/Practica 3.cs
@@ -5,43 +5,42 @@ namespace Practica_3
     {
         public static void Main(string[] args)
         {
-        
             //Constantes
-            int porcentajeDescuento=20;
-        
+            int porcentajeDescuento = 20;
+
             //variables de lectura de datos
             string nombre, direccion, mes;
-            int kw =0,valor_kw =0;
-        
+            int kw = 0, valor_kw = 0;
+
             //Variables de operacion
-            float totalPagar = 0,subTotal = 0, valorDescuento=0,valorConsumo=0;
+            float totalPagar = 0, subTotal = 0, valorDescuento = 0, valorConsumo = 0;
 
             //Lectura de la información suministrada por el usuario
             Console.WriteLine("*** PROGRAMA DE FACTURA DE CONSUMO DEL SERVICIO DE ENERGÍA ***");
             Console.WriteLine("***                ELECTRIFICADORA AZUL                ***\n");
-        
+
             Console.WriteLine("* Escriba el nombre del propietario de la vivienda:");
-            nombre=Console.ReadLine();
-        
+            nombre = Console.ReadLine();
+
             Console.WriteLine("\n* Escriba la dirección de la vivienda:");
-            direccion=Console.ReadLine();		
+            direccion = Console.ReadLine();
 
             Console.WriteLine("\n* Escriba el mes del recibo facturado:");
-            mes=Console.ReadLine(); 
-        
+            mes = Console.ReadLine();
+
             Console.WriteLine("\n* Escriba el numero de KW de este mes:");
             kw = Convert.ToInt32(Console.ReadLine());
-        
+
             //Calcular Precio de Kw
             if (kw < 501)
             {
                 valor_kw = 15;
             }
-            if (kw > 500 && kw <1001)
+            if (kw > 500 && kw < 1001)
             {
                 valor_kw = 25;
             }
-            if (kw > 1000 && kw <1501)
+            if (kw > 1000 && kw < 1501)
             {
                 valor_kw = 30;
             }
@@ -49,24 +48,24 @@ namespace Practica_3
             {
                 valor_kw = 35;
             }
-        
+
             //Calcular Valor Cosumo
-            valorConsumo= valor_kw * kw;
-        
+            valorConsumo = valor_kw * kw;
+
             //Calcular Total a Pagar
             if (kw < 1000)
             {
-                valorDescuento = (valorConsumo * porcentajeDescuento ) / 100;
-                subTotal=valorConsumo;
+                valorDescuento = (valorConsumo * porcentajeDescuento) / 100;
+                subTotal = valorConsumo;
                 totalPagar = valorConsumo - valorDescuento;
             }
-        
+
             if (kw > 1000)
             {
-                subTotal=valorConsumo;
+                subTotal = valorConsumo;
                 totalPagar = valorConsumo;
             }
-        
+
             //Salida de los datos según el requerimiento
             Console.WriteLine("\n*** RECIBO DE CONSUMO DEL SERVICIO DE ENERGÍA ***\n");
 
