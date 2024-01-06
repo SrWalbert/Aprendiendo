336f33b24fb743c76a692b787a987a2ffaaf663c
SrWalbert <walberttrejo@gmail.com>
2024-01-06 16:11:36 -0700

    Reparacion y buenas prácticas

--- C#/Escuela/Segundo parcial/Kardex escuela/Kardex.py
+++ C#/Escuela/Segundo parcial/Kardex escuela/Kardex.py
@@ -1,70 +1,47 @@
 # El c# está muy feo de trabajar, voy a usar python, si no le molesta
-
-# Capturando datos
-from numpy import var
+import sys
 
 
-while True:
-    print("Introduce tus datos. \n")
-    try:
-        nombre_de_la_escuela = input("Ingresa el nombre de tu escuela: ")
-        nombre_del_alumno = input("Igresa tu nombre completo: ")
-        edad_del_alumno = input("Ingresa tu edad: ")
-        grado_del_alumno = input("Ingresa tu grado: ")
-        grupo_del_alumno = input("Ingresa tu grupo: ")
-        periodo_del_alumno = input("Igresa tu periodo, el NÚMERO de semestre: ")
-        mes = input("En qué mes estás: ")
-        materias = ("español", "matematicas", "ingles", "quimica", "fisica")
-        calificacion_ = {}
-        for materia in materias:
-            calificacion_[materia] = input(
-                f"Ingresa tu calificación de {materia} del uno al diez: "
+def main(*args: None) -> int:
+    while True:
+        print("Introduce tus datos.\n")
+        try:
+            nombre_de_la_escuela = input("Ingresa el nombre de tu escuela: ")
+            nombre_del_alumno = input("Ingresa tu nombre completo: ")
+            edad_del_alumno = int(input("Ingresa tu edad: "))
+            grado_del_alumno = input("Ingresa tu grado: ")
+            grupo_del_alumno = input("Ingresa tu grupo: ")
+            periodo_del_alumno = input("Ingresa tu periodo, el NÚMERO de semestre: ")
+            mes = input("En qué mes estás: ")
+
+            materias = ("español", "matematicas", "ingles", "quimica", "fisica")
+            calificaciones = {}
+            for materia in materias:
+                calificaciones[materia] = float(
+                    input(f"Ingresa tu calificación de {materia} del uno al diez: ")
+                )
+
+            promedio = sum(calificaciones.values()) / len(materias)
+
+            estatus_del_estudiante: str = "Aprobado" if promedio >= 6 else "Reprobado"
+
+            print(
+                f"\n---------------------\nNombre del alumno: {nombre_del_alumno}\nEdad:{edad_del_alumno}\nGrado: {grado_del_alumno}\nGrupo: {grupo_del_alumno}\nPeriodo {periodo_del_alumno}\nMes actual: {mes}\n\n\nEstado: {estatus_del_estudiante}"
             )
-            calificacion_[materia] = float(calificacion_[materia])
-        
-        calificacion_.keys() = calificacion_.values()
-        # calificacion_español = input(
-        #     "Ingresa tu calificación de español del uno al diez: "
-        # )
-        # calificacion_matematicas = input(
-        #     "Ingresa tu calificación de matematicas del uno al diez: "
-        # )
-        # calificacion_ingles = input(
-        #     "Ingresa tu calificación de inglés del uno al diez: "
-        # )
-        # calificacion_quimica = input(
-        #     "Ingresa tu calificación de química del uno al diez: "
-        # )
-        # calificacion_fisica = input(
-        #     "Ingresa tu calificación de física del uno al diez: "
-        # )
 
-        # Convertir a numeros
-        edad_del_alumno = int(edad_del_alumno)
-    except Exception as e:
-        print("Algo salió mal, no metiste tus datos correctamente, inténtalo de nuevo")
-        print(f"Tipo de error: {e}")
-    else:
-        break
+            for materia, calificacion in calificaciones.items():
+                print(f"{materia.capitalize()}: {calificacion}")
+
+            print(f"\nPromedio: {promedio}\nEstado: {estatus_del_estudiante}")
+
+            return 0
+
+        except ValueError as e:
+            print(
+                "Algo salió mal, no metiste tus datos correctamente, inténtalo de nuevo"
+            )
+            print(f"Tipo de error: {e}")
 
 
-# Operaciones para sacar el promedio
-promedio = (
-    calificacion_español
-    + calificacion_matematicas
-    + calificacion_fisica
-    + calificacion_quimica
-    + califcacion_ingles
-) / 5
-
-# Rebrobar  o no
-if promedio >= 6:
-    estatus_del_estudiante = "Aprobado"
-else:
-    estatus_del_estudiante = "Reprobado"
-
-# Mostrar información
-
-print(
-    f"\n---------------------\nNombre del alumno: {nombre_del_alumno}\nEdad:{edad_del_alumno}\nGrado: {grado_del_alumno}\nGrupo: {grupo_del_alumno}\nPeriodo {periodo_del_alumno}\nMes actual: {mes}\n\n\nEspañol. {español}\nMatematicas: {matematicas}\nInglés: {ingles}\nQuímica: {quimica}\nFísica {fisica}\n\nPromedio: {promedio}\nEstado: {estatus_del_estudiante}"
-)
+if __name__ == "__main__":
+    sys.exit(main())
