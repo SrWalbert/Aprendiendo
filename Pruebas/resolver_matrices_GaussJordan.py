f4f03d9778912d0d61d0724be52356b7629fd331
walbert <walberttrejo@gmail.com>
2024-11-03 15:07:39 -0700

    comprobation

--- Pruebas/resolver_matrices_GaussJordan.py
+++ Pruebas/resolver_matrices_GaussJordan.py
@@ -1,43 +1,58 @@
 import numpy as np
 import sys
 
-# Definimos la matriz aumentada del sistema
-augmented_matrix = np.array(
-    [
-        [2, 3, 2, 1400],
-        [1, 4, 1, 1100],
-        [1, 2, 3, 1300],
-    ],
-    dtype=float,
-)
+np.set_printoptions(suppress=True)
 
 
 # Aplicamos el método de Gauss-Jordan para reducir la matriz aumentada a forma escalonada reducida
 def gauss_jordan_elimination(matrix):
     rows, cols = matrix.shape
-
     for i in range(rows):
         # Hacer que el pivote sea 1 dividiendo toda la fila por el elemento pivote
         pivot = matrix[i, i]
         matrix[i] = matrix[i] / pivot
-
         # Hacer ceros en la columna del pivote para las otras filas
         for j in range(rows):
             if j != i:
                 factor = matrix[j, i]
                 matrix[j] = matrix[j] - factor * matrix[i]
-
     return matrix
 
 
+def comprobation(original_matrix, solution_vector) -> bool:
+    original_rows, original_cols = original_matrix.shape
+    original_solutions = original_matrix[:, -1]
+    for i in range(original_rows):
+        for j in range(original_cols - 1):
+            for k in solution_vector:
+                if j * k in [solution for solution in original_solutions]:
+                    continue
+                else:
+                    return False
+    return True
+
+
 def main(*args: None) -> int:
+    # Definimos la matriz aumentada del sistema
+    augmented_matrix = np.array(
+        # fmt: off
+        [
+            [2, 3, 2, 1400],
+            [1, 4, 1, 1100],
+            [1, 2, 3, 1300],
+
+        ],
+        # fmt: on
+        dtype=float,
+    )
     # Ejecutamos el método de Gauss-Jordan
     reduced_matrix = gauss_jordan_elimination(augmented_matrix.copy())
     print(reduced_matrix)
     # Extraemos las soluciones para X1, X2 y X3
     solutions = reduced_matrix[:, -1]
-    print(solutions)
+    print("Soluciones: ", solutions)
 
+    print("Está correcto", comprobation(augmented_matrix, solutions))
     return 0
 
 
