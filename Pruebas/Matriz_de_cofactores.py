839a67916f4214e33317f5e40903a69530901334
walbert <walberttrejo@gmail.com>
2024-11-26 11:29:01 -0700

    Formato

--- Pruebas/Matriz_de_cofactores.py
+++ Pruebas/Matriz_de_cofactores.py
@@ -1,10 +1,11 @@
+import sys
 import sympy as sp
-
 
 # Definición de la matriz original
 A = sp.Matrix(
     # fmt: off
     [
+        
         [1, -2, 1, 2],
         [3, -1, -5, 4], 
         [-4, 3, 2, 4],
@@ -13,6 +14,26 @@ A = sp.Matrix(
     # fmt:on
 )
 
-# Cálculo de la matriz de cofactores
-print(A.cofactor_matrix())
-print(A._eval_determinant())
+B = sp.Matrix(
+    # fmt: off
+    [
+        [3,7],
+        [2,5]
+    ]
+    # fmt:on
+)
+
+
+def main(*args: None) -> int:
+    # Cálculo de la matriz de cofactores
+    print(A.cofactor_matrix())
+    print(A._eval_determinant())
+    print(A**-1)
+    print("-------------------------")
+    # B
+    print(B**-1)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
